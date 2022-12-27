c3f263e3ca451a024fcb2824ceb1085cd56a836d
eliyahukoren <elik.koren@gmail.com>
2022-12-27 11:31:10 +0200

    add code

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-/* Happy hacking !*/
+/**
+ * @param {string[]} garbage
+ * @param {number[]} travel
+ * @return {number}
+ */
+module.exports = garbageCollection = function (garbage, travel) {
+	const stack = [];
+	let time = 0;
+	let total = 0;
+
+	garbage.forEach((g, i) => {
+		let type = g.split("");
+		time = i > 0 ? (time += travel[i - 1]) : 0;
+
+		type.forEach((t) => {
+			if (!stack[t]) {
+				stack[t] = { travel: time, pick: 1 };
+			} else {
+				stack[t].travel = time;
+				stack[t].pick += 1;
+			}
+		});
+	});
+
+	for (let k in stack) {
+		total += stack[k].travel + stack[k].pick;
+	}
+
+	return total;
+};
--- test/index.test.js
+++ test/index.test.js
@@ -1,1 +1,9 @@
 const assert = require('assert');
+const garbageCollection = require("../index");
+
+describe("Running basic test", () => {
+	it("Basic test", () => {
+		assert.equal(garbageCollection(["G", "P", "GP", "GG"], [2, 4, 3]), 21);
+		assert.equal(garbageCollection(["MMM", "PGM", "GP"], [3, 10]), 37);
+	})
+})
